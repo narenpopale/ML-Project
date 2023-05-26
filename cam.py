e942f0d37c2e6e5b2c12225cbcdaf6a9b1a33199
naren <91795163+narenpopale@users.noreply.github.com>
2023-05-26 09:01:40 +0530

    Add files via upload

---
+++ cam.py
@@ -0,0 +1,12 @@
+import os
+
+import cvzone
+from cvzone.ClassificationModule import Classifier
+import cv2  
+
+cap = cv2.VideoCapture(0)
+    
+while True:
+    _, img = cap.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
